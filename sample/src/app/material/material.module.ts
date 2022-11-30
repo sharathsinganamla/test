871070af28add7de34b06f0950e6a6319f9d6203
sharathsinganamla <53082629+sharathsinganamla@users.noreply.github.com>
2022-11-30 13:58:21 +0530

    Add files via upload

---
+++ sample/src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+//import { CommonModule } from '@angular/common';
+import { MatSliderModule } from '@angular/material/slider';
+//import {MatSliderModule} from '@angular/material/slider';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSliderModule
+  ]
+})
+export class MaterialModule { }
